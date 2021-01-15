22164c5cde3553a9f11838ff02ab8d757b9e3e22
Lorena Ramirez <ramirezlorenaa18@gmail.com>
2021-01-15 15:10:40 -0500

    created footer

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+          <div className ="container">
+            <p className = "text-muted"style={{textAlign:"center"}}> Graduates UNCC Coding Bootcamp 1/27/2021</p>
+          </div>
+        </footer>
+    )
+}
+
+export default Footer; 

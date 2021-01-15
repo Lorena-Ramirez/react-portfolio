9c609228adc97e1a58be11f444d729b11eab7f1a
Lorena Ramirez <ramirezlorenaa18@gmail.com>
2021-01-15 09:46:42 -0500

    created base for contact page

---
+++ src/components/pages/Contact.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "../../style.css";
+
+function Contact() {
+  return (
+    <div>
+      <h1>Contact Page</h1>
+      <p>
+        
+      </p>
+    </div>
+  );
+}
+
+export default Contact;

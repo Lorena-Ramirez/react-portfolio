a3b1c8666606b3ec7bf4d42051bba4d5a1a67ec3
Lorena Ramirez <ramirezlorenaa18@gmail.com>
2021-01-15 15:12:52 -0500

    created contact page with links to github, linkedin, and email

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -3,12 +3,30 @@ import "../../style.css";
 
 function Contact() {
   return (
-    <div>
-      <h1>Contact Page</h1>
-      <p>
-        
-      </p>
-    </div>
+  <div>
+    <section>
+      <div className="container my-container page-container">
+        <div className="col shadow p-3 rounded aboutMe-block">
+          <div className="row">
+            <div className="col" style={{marginTop: "auto",marginBottom: "auto"}}>
+              <h1 style={{textAlign:"center"}}> Contact Me</h1>
+            </div>
+        </div>
+          
+          <div className="row">
+            <div className="col" style ={{wordBreak:"break-all"}}>
+              <h2 style={{textAlign:"center"}}>
+              GitHub: <a href="https://github.com/Lorena-Ramirez" target="_blank" rel="noreferrer">Github-Lorena-Ramirez</a> <br></br>
+              LinkedIn: <a href="https://www.linkedin.com/in/lorena-ramirez-bb788a1b8/"
+              target="_blank" rel="noreferrer">LinkIn-Lorena-Ramirez</a><br></br>
+              Email: <a href="mailto:ramirezlorenaa18@gmail.com">ramirezlorenaa18@gmail.com</a>
+              </h2>
+            </div>
+         </div>
+        </div>
+      </div>
+    </section>
+  </div>
   );
 }
 

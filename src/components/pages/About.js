25e5f3ed6a3d991440b5ec32114b8d9eb7a7d438
Lorena Ramirez <ramirezlorenaa18@gmail.com>
2021-03-16 21:03:55 -0400

    updated resume and fixed buttons

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ProfilePic from "../../assets/images/profile-pic.jpeg"
-import Resume from "../../assets/resume/Lorena's_Resume.pdf"
+import Resume from "../../assets/resume/_Lorena Ramirez- Full-Stack Web Developer Resume.pdf"
 import { Link } from "react-router-dom";
 import "../../style.css";
 
@@ -23,27 +23,23 @@ function About() {
               <img src={ProfilePic} alt="Profile of Lorena Ramirez"
                 className="image-thumbnail float-left mr-3" />
 
-              <p className="bio-text">Hi there! My name is Lorena Ramirez. I am 19 years old and I am a currently enrolled in
-              the UNCC coding bootcamp! I graduated from Clover High
-              School in 2018. I also attended the UNCC for alittle over a year. I majored in computer science however,
-              decided to do the coding bootcamp first
-              to get my foot in the door of the developer world! I plan to complete my degree after the coding bootcamp
-              to accumulate more experience under my belt.
-              I am also currently learning Japanese. I have a passion for the culture and the language and hope to live
-              there one day for a period of my life.
+              <p className="bio-text">Hi there! My name is Lorena Ramirez. I am 19 years old and I recently graduated
+              UNCC's Full-Stack Coding Bootcamp! I am currently employeed through a part-time internship with the fitness app Quictivity where I
+              am a react developer working on front and back-end development. As a skilled Full-Stack React Developer I craft responsive and performant web applications.
+              I offer a variety of technical skills acquired from my experience with graduating UNCC's Full-Stack Coding Bootcamp as well as personal projects I've worked on in my free time.
+              Working with a team in group projects has granted me a professional reputation of leadership and effective communication skills.
+              My adaptability has aided in my ability to identify issues, brainstorm and troubleshoot problems, and evaluate effective solutions.
               </p>
 
               <p className="bio-text">
                 I am very passionate about coding and have a drive to learn more skills everyday. I thoroughly enjoy the
                 coding bootcamp and hope to pursue a succesful career
                 along the way. Outside of coding my hobbies include listening to audiobooks and hiking. In my freetime I
-                enjoy spending time with my family. I am
-                extremely family-oriented which also fueled my passion for the tech industries in hopes of landing a job
-                fully remote!
-
+                enjoy spending time with my family. Thank you for taking the time to get to know me.
+                 I hope you enjoy ezploring my projects I've been working on and that we get in contact soon!
               <div className="flex">
                   <button type="button" className="btn modal-btn" >
-                  <Link className="resume-link" to="/contact">Get connected with me! </Link>
+                    <Link className="resume-link" to="/contact">Get connected with me! </Link>
                   </button>
 
                   <button className="btn modal-btn">
